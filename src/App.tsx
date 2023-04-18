994a71a3544b79de28eaf32f437f160de1db2f6e
mikolaj.wittbrodt <m.witt@int.pl>
2023-04-18 21:51:53 +0200

    Preparing for adding top navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
+import NavigationTopBar from './components/NavigationTopBar';
+import styled from 'styled-components';
+import colors from './theme/colors';
+
+const Wrapper = styled.div`
+  display: flex;
+  background-color: ${colors.primaryBackground};
+  color: ${colors.primaryText};
+`;
 
 function App() {
   return (
-    <div>
-      <header>
-
-      </header>
-    </div>
+    <Wrapper>
+      <NavigationTopBar />
+    </Wrapper>
   );
 }
 
---
+++ src/components/NavigationTopBar.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import styled from 'styled-components';
+import colors from '../theme/colors';
+
+const Wrapper = styled.div``;
+const StyledParagraph = styled.p`
+  margin: 0;
+`;
+
+const NavigationTopBar = () => {
+  return (
+    <Wrapper>
+      <StyledParagraph>dupa</StyledParagraph>
+    </Wrapper>
+  );
+};
+
+export default NavigationTopBar;
