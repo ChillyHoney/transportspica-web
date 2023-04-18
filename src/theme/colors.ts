4462bfb4db396c19901bb926e21f5ca067f1f775
mikolaj.wittbrodt <m.witt@int.pl>
2023-04-18 21:50:51 +0200

    Adding color pallete

---
+++ src/theme/colors.ts
@@ -0,0 +1,19 @@
+interface Colors {
+  primaryBackground: string;
+  secondaryBackground: string;
+  thirdBackground: string;
+  primaryFunctional: string;
+  secondaryFunctional: string;
+  primaryText: string;
+}
+
+const colors: Colors = {
+  primaryBackground: '#000814',
+  secondaryBackground: '#001d3d',
+  thirdBackground: '#003566',
+  primaryFunctional: '#ffc300',
+  secondaryFunctional: '#ffd60a',
+  primaryText: '#f5f5f5',
+};
+
+export default colors;
